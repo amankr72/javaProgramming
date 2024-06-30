c5b281d0e9efa1dcb0d64a3f1fd518e84caf03b7
amankr72 <amankrgaya7@gmail.com>
2024-06-30 11:50:51 +0530

    Add files via upload

---
+++ withoutstring.java
@@ -0,0 +1,22 @@
+public class withoutstring {
+    public static void main(String[] args) {
+        // int number = 12;
+        //       number-= 6;
+        //             System.out.println(number);
+        //not operator
+        //boolean isStudent = false ;
+        //boolean isLibraryMember = false;
+        // System.out.println(!isStudent);
+       //String
+        //String name= new String("aman");
+        //System.out.println(name);
+        //String literalString1 = "abc";
+        //String literalString2 ="abc";
+
+        //String objectString1 = new String ("xyz");
+        //String objectString2 = new String ("xyz" );
+
+        //System.out.println(literalString1 == literalString2);
+        //System.out.println(objectString1==objectString2);
+    }
+}
